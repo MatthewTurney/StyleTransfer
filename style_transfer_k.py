8f39e67a1ddf011aa0d6f08acec5124601377e7e
MatthewTurney <matthew.w.turney@gmail.com>
2018-06-02 10:17:00 -0700

    updates

--- style_transfer_k.py
+++ style_transfer_k.py
@@ -100,13 +100,13 @@ evaluator=Evaluator()
 
 x=np.random.uniform(0,255,(1,height,width,3))-128.0
 
-iterations = 5
+iterations = 10
 import time
 for i in range(iterations):
     print('Start of iteration', i)
     start_time = time.time()
     x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
-                           fprime=evaluator.grads, maxfun=10)
+                           fprime=evaluator.grads, maxfun=20)
     print(min_val)
     end_time = time.time()
     print('Iteration %d completed in %ds' % (i, end_time - start_time))
